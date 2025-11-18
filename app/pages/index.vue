bac8a9d5bfcdd52292712accc3eed90be1975efc
Ibrahim H. <bitsnaps@yahoo.fr>
2025-11-18 17:15:32 +0000

    update contact

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,6 +2,8 @@
 </script>
 <template>
     <div>
-        <h1>Coming Soon...</h1>
+        <h1>الموقع قيد طور الإنجاز...</h1>
+        <p>Email: <a href="mailto:contact@aliyaat.net">contact@aliyaat.net</a></p>
+        <p>Tel: +213 (0) 555 123 456</p>
     </div>    
 </template>
