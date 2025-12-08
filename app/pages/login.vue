60af4e13d303a297d1cace8b55cbdb7e4c83a82c
Ibrahim H <bitsnaps@yahoo.fr>
2025-12-08 10:59:02 +0100

    fix(login): response condition

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -22,7 +22,7 @@ async function login() {
       method: 'POST',
       body: state
     });
-    if (!response && response.statusCode !== 200) {
+    if (!response || response.statusCode !== 200) {
       toast.add({ title: 'Error', description: response?.message || 'Access denied', color: 'warning' })
     } else {      
       const { login: authLogin } = useAuth()
