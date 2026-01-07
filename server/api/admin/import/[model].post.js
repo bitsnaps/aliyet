598b6466fa608b9cb4242f5b7355a0d024810cc9
Ibrahim H <bitsnaps@yahoo.fr>
2026-01-07 18:58:56 +0100

    fix(import): refine xlsx dynamic import for esm

--- server/api/admin/import/[model].post.js
+++ server/api/admin/import/[model].post.js
@@ -12,7 +12,8 @@ export default defineEventHandler(async (event) => {
     let XLSX;
     try {
       addLog('Attempting to dynamically import xlsx...');
-      const _xlsx = await import('xlsx');
+      //const _xlsx = await import('xlsx');
+      const _xlsx = await import('xlsx/xlsx.mjs');
       XLSX = _xlsx.default || _xlsx;
       addLog('xlsx imported successfully.');
     } catch (e) {
